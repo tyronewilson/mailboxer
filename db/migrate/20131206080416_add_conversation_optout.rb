c07c38a6699b52b42c50e07fa615ddc20a3e0fab
Tyrone <tdubs241083@gmail.com>
2014-07-12 07:17:15 +0200

    Update 20131206080416_add_conversation_optout.rb
    remove comment which is auto generated

--- db/migrate/20131206080416_add_conversation_optout.rb
+++ db/migrate/20131206080416_add_conversation_optout.rb
@@ -1,4 +1,3 @@
-# This migration comes from mailboxer_engine (originally 20131206080416)
 class AddConversationOptout < ActiveRecord::Migration
   def change
     create_table :mailboxer_conversation_opt_outs do |t|

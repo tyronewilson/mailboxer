133f75bd75049d3bd022d7c6cd9cf2bf5b34b4a6
Tyrone <tdubs241083@gmail.com>
2014-07-12 07:10:00 +0200

    rearrange migrations to work with schema_plus

--- db/migrate/20110511145103_create_mailboxer.rb
+++ db/migrate/20110511145103_create_mailboxer.rb
@@ -1,65 +1,50 @@
+# This migration comes from mailboxer_engine (originally 20110511145103)
 class CreateMailboxer < ActiveRecord::Migration
-  def self.up    
+  def change
   #Tables
-  	#Conversations
+
+    #Conversations
     create_table :mailboxer_conversations do |t|
-      t.column :subject, :string, :default => ""
-      t.column :created_at, :datetime, :null => false
-      t.column :updated_at, :datetime, :null => false
-    end    
-  	#Receipts
-    create_table :mailboxer_receipts do |t|
-      t.references :receiver, :polymorphic => true
-      t.column :notification_id, :integer, :null => false
-      t.column :is_read, :boolean, :default => false
-      t.column :trashed, :boolean, :default => false
-      t.column :deleted, :boolean, :default => false
-      t.column :mailbox_type, :string, :limit => 25
-      t.column :created_at, :datetime, :null => false
-      t.column :updated_at, :datetime, :null => false
-    end    
-  	#Notifications and Messages
+      t.string :subject, :default => ""
+      t.datetime :created_at, :null => false
+      t.datetime :updated_at, :null => false
+    end
+
+    #Notifications and Messages
     create_table :mailboxer_notifications do |t|
-      t.column :type, :string
-      t.column :body, :text
-      t.column :subject, :string, :default => ""
+      t.string :type
+      t.text :body
+      t.string :subject, :default => ""
       t.references :sender, :polymorphic => true
-      t.column :conversation_id, :integer
-      t.column :draft, :boolean, :default => false
+      t.integer :conversation_id, foreign_key: {references: :mailboxer_conversations, name: 'notifications_on_conversation_id'}
+      t.boolean :draft, :default => false
       t.string :notification_code, :default => nil
       t.references :notified_object, :polymorphic => true
-      t.column :attachment, :string
-      t.column :updated_at, :datetime, :null => false
-      t.column :created_at, :datetime, :null => false
+      t.string :attachment
+      t.datetime :updated_at, :null => false
+      t.datetime :created_at, :null => false
       t.boolean :global, default: false
       t.datetime :expires
-    end    
-    
-    
-  #Indexes
-  	#Conversations
-  	#Receipts
-  	add_index "mailboxer_receipts","notification_id"
+    end
 
-  	#Messages  
-  	add_index "mailboxer_notifications","conversation_id"
-  
-  #Foreign keys    
-  	#Conversations
-  	#Receipts
-  	add_foreign_key "mailboxer_receipts", "mailboxer_notifications", :name => "receipts_on_notification_id", :column => "notification_id"
-  	#Messages  
-  	add_foreign_key "mailboxer_notifications", "mailboxer_conversations", :name => "notifications_on_conversation_id", :column => "conversation_id"
-  end
-  
-  def self.down
-  #Tables  	
-  	remove_foreign_key "mailboxer_receipts", :name => "receipts_on_notification_id"
-  	remove_foreign_key "mailboxer_notifications", :name => "notifications_on_conversation_id"
-  	
+    #Receipts
+    create_table :mailboxer_receipts do |t|
+      t.references :receiver, :polymorphic => true
+      t.integer :notification_id, :null => false, foreign_key: {references: :mailboxer_notifications, name: 'receipts_on_notification_id'}
+      t.boolean :is_read, :default => false
+      t.boolean :trashed, :default => false
+      t.boolean :deleted, :default => false
+      t.string :mailbox_type, :limit => 25
+      t.datetime :created_at, :null => false
+      t.datetime :updated_at, :null => false
+    end
+
   #Indexes
-    drop_table :mailboxer_receipts
-    drop_table :mailboxer_conversations
-    drop_table :mailboxer_notifications
+    #Conversations
+    #Receipts
+    add_index "mailboxer_receipts","notification_id"
+
+    #Messages
+    add_index "mailboxer_notifications","conversation_id"
   end
 end
--- db/migrate/20131206080416_add_conversation_optout.rb
+++ db/migrate/20131206080416_add_conversation_optout.rb
@@ -1,14 +1,9 @@
+# This migration comes from mailboxer_engine (originally 20131206080416)
 class AddConversationOptout < ActiveRecord::Migration
-  def self.up
+  def change
     create_table :mailboxer_conversation_opt_outs do |t|
       t.references :unsubscriber, :polymorphic => true
-      t.references :conversation
+      t.references :conversation, foreign_key: {references: :mailboxer_conversations, name: 'mb_opt_outs_on_conversations_id'}
     end
-    add_foreign_key "mailboxer_conversation_opt_outs", "mailboxer_conversations", :name => "mb_opt_outs_on_conversations_id", :column => "conversation_id"
-  end
-
-  def self.down
-    remove_foreign_key "mailboxer_conversation_opt_outs", :name => "mb_opt_outs_on_conversations_id"
-    drop_table :mailboxer_conversation_opt_outs
   end
 end
